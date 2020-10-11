6941e092bed2a3d243fb46036504aac7a8b67369
victor.penit <vpen01@ucm.es>
2020-10-11 20:02:16 +0200

    commit inicial

---
+++ src/main/java/juego/cartas/blackjack/JuegoCartasBlackJackApplication.java
@@ -0,0 +1,13 @@
+package juego.cartas.blackjack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuegoCartasBlackJackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuegoCartasBlackJackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/juego/cartas/blackjack/JuegoCartasBlackJackApplicationTests.java
@@ -0,0 +1,13 @@
+package juego.cartas.blackjack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuegoCartasBlackJackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
